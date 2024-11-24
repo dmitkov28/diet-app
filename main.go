63b45ba5d05e0c4ce2fde8abcd2e31be6a397377
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-11-24 11:51:12 +0200

    add basic scaffolding

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-func main(){}
+import (
+	"context"
+
+	"github.com/a-h/templ"
+	"github.com/dmitkov28/dietapp/templates"
+	"github.com/labstack/echo/v4"
+)
+
+func render(c echo.Context, component templ.Component) error {
+	return component.Render(context.Background(), c.Response().Writer)
+}
+
+func main() {
+	e := echo.New()
+	e.Static("/static", "static")
+	e.GET("/", func(c echo.Context) error {
+		return render(c, templates.HomePage())
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
