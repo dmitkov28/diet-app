5ffae98a2bc4cb27d62b14a8263dd657424d24ab
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-11-27 17:42:12 +0200

    update settings schema

--- data/schema/003_settings.sql
+++ data/schema/003_settings.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS settings (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     current_weight REAL NOT NULL,
     target_weight REAL NOT NULL,
-    goal_deadline DATE NOT NULL,
+    target_weight_loss_rate REAL NOT NULL,
     user_id INT NOT NULL UNIQUE,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
