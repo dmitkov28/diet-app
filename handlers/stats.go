da0547d1318afdc4e4b114f621701ffb545fba1c
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-11-29 11:56:11 +0200

    update stats handler

--- handlers/stats.go
+++ handlers/stats.go
@@ -1,12 +1,20 @@
 package handlers
 
 import (
+	"fmt"
+
+	"github.com/dmitkov28/dietapp/data"
 	"github.com/dmitkov28/dietapp/templates"
 	"github.com/labstack/echo/v4"
 )
 
-func StatsGETHandler() echo.HandlerFunc {
+func StatsGETHandler(repo *data.MeasurementRepository) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return render(c, templates.StatsPage(88))
+		userId := c.Get("user_id").(int)
+		items, err := repo.GetMeasurementsByUserId(userId)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return render(c, templates.StatsPage(89, items))
 	}
 }
