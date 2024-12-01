473f038fe680f252cb4ce7b6aca0c9fe46617c19
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-12-01 08:51:20 +0200

    make weight unique with user

--- data/schema/001_weight.sql
+++ data/schema/001_weight.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS weight (
     date DATE NOT NULL,
     weight REAL,
     user_id INT NOT NULL,
+    UNIQUE(date, user_id) ON CONFLICT REPLACE,
     FOREIGN KEY(user_id) REFERENCES users(id)
     ON DELETE CASCADE
 );
