545b777433428ccdc4911afab0e51e48db4d743a
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-12-07 12:31:19 +0200

    remove unused queries

--- data/measurement.go
+++ data/measurement.go
@@ -41,44 +41,6 @@ func NewMeasurementsRepository(db *DB) *MeasurementRepository {
 	return &MeasurementRepository{db: db}
 }
 
-func (repo *MeasurementRepository) GetWeightByUserId(userId int) ([]Weight, error) {
-	rows, err := repo.db.db.Query("SELECT weight, date FROM weight WHERE user_id = ?", userId)
-	if err != nil {
-		return []Weight{}, err
-	}
-	defer rows.Close()
-	var result []Weight
-	for rows.Next() {
-		var row Weight
-		err := rows.Scan(&row.Weight, &row.Date)
-		if err != nil {
-			return []Weight{}, err
-		}
-		result = append(result, row)
-	}
-	return result, nil
-
-}
-
-func (repo *MeasurementRepository) GetWeightsBetweenDates(userId int, startDate, endDate string) ([]Weight, error) {
-	rows, err := repo.db.db.Query("SELECT weight, date FROM weight WHERE user_id = ? AND date BETWEEN ? AND ?", userId, startDate, endDate)
-	if err != nil {
-		return []Weight{}, err
-	}
-	defer rows.Close()
-	var result []Weight
-	for rows.Next() {
-		var row Weight
-		err := rows.Scan(&row.Weight, &row.Date)
-		if err != nil {
-			return []Weight{}, err
-		}
-		result = append(result, row)
-	}
-	return result, nil
-
-}
-
 func (repo *MeasurementRepository) CreateWeight(weight Weight) (Weight, error) {
 	res, err := repo.db.db.Exec("INSERT INTO weight(user_id, weight, date) VALUES(?, ?, ?)", weight.User_id, weight.Weight, weight.Date)
 	if err != nil {
@@ -97,44 +59,6 @@ func (repo *MeasurementRepository) CreateWeight(weight Weight) (Weight, error) {
 		return Weight{}, err
 	}
 	return newWeight, nil
-
-}
-
-func (repo *MeasurementRepository) GetCaloriesByUserId(userId int) ([]Calories, error) {
-	rows, err := repo.db.db.Query("SELECT calories, date FROM calories WHERE user_id = ?", userId)
-	if err != nil {
-		return []Calories{}, err
-	}
-	defer rows.Close()
-	var result []Calories
-	for rows.Next() {
-		var row Calories
-		err := rows.Scan(&row.Calories, &row.Date)
-		if err != nil {
-			return []Calories{}, err
-		}
-		result = append(result, row)
-	}
-	return result, nil
-
-}
-
-func (repo *MeasurementRepository) GetCaloriesBetweenDates(userId int, startDate, endDate string) ([]Calories, error) {
-	rows, err := repo.db.db.Query("SELECT calories, date FROM calories WHERE user_id = ? AND date BETWEEN ? AND ?", userId, startDate, endDate)
-	if err != nil {
-		return []Calories{}, err
-	}
-	defer rows.Close()
-	var result []Calories
-	for rows.Next() {
-		var row Calories
-		err := rows.Scan(&row.Calories, &row.Date)
-		if err != nil {
-			return []Calories{}, err
-		}
-		result = append(result, row)
-	}
-	return result, nil
 
 }
 
@@ -233,95 +157,6 @@ func (repo *MeasurementRepository) GetMeasurementsByUserId(userId, offset int) (
 	}
 
 	return results, nil
-}
-
-func (repo *MeasurementRepository) GetMeasurementsBetweenDates(userId int, startDate, endDate string) ([]WeightCalories, error) {
-	query := `
-        SELECT 
-            w.id, 
-            w.weight, 
-            w.date as weight_date,
-            c.id as calories_id,
-            c.calories,
-            c.date as calories_date,
-            w.user_id
-        FROM weight w
-        LEFT JOIN calories c ON w.date = c.date AND w.user_id = c.user_id
-        WHERE w.user_id = ? AND w.date BETWEEN ? AND ?
-        ORDER BY w.date DESC`
-
-	rows, err := repo.db.db.Query(query, userId, startDate, endDate)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var results []WeightCalories
-	for rows.Next() {
-		var wc WeightCalories
-		var caloriesID sql.NullInt64
-		var calories sql.NullFloat64
-		var caloriesDate sql.NullString
-
-		err := rows.Scan(
-			&wc.WeightID,
-			&wc.Weight,
-			&wc.WeightDate,
-			&caloriesID,
-			&calories,
-			&caloriesDate,
-			&wc.UserID,
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		if caloriesID.Valid {
-			val := int(caloriesID.Int64)
-			wc.CaloriesID = &val
-		}
-		if calories.Valid {
-			val := int(calories.Float64)
-			wc.Calories = &val
-		}
-		if caloriesDate.Valid {
-			wc.CaloriesDate = &caloriesDate.String
-		}
-
-		results = append(results, wc)
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return results, nil
-}
-
-func (repo *MeasurementRepository) GetCurrentWeightAvg(userId int) (float64, error) {
-	query := `
-	SELECT 
-    	AVG(CASE WHEN date >= date('now', '-6 days') THEN weight END) AS current_week_avg
-	FROM weight
-	WHERE user_id = ?
-	`
-
-	rows, err := repo.db.db.Query(query, userId)
-	if err != nil {
-		return 0, err
-	}
-
-	defer rows.Close()
-
-	var currentWeekAvg float64
-
-	for rows.Next() {
-		err := rows.Scan(&currentWeekAvg)
-		if err != nil {
-			return 0, err
-		}
-	}
-	return currentWeekAvg, nil
 }
 
 func (repo *MeasurementRepository) DeleteWeightAndCaloriesByWeightID(weightID string) error {
