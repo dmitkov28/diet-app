b2a7a4cbeb4009249c735efafc920ec51fdf193e
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-12-14 19:56:44 +0200

    add food log handlers

--- handlers/food_log.go
+++ handlers/food_log.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/dmitkov28/dietapp/data"
+	"github.com/dmitkov28/dietapp/diet"
 	"github.com/dmitkov28/dietapp/templates"
 	"github.com/labstack/echo/v4"
 )
@@ -28,12 +31,57 @@ func FoodLogGETHandler(repo *data.FoodLogRepository, settingsRepo *data.Settings
 func FoodLogPOSTHandler(repo *data.FoodLogRepository, settingsRepo *data.SettingsRepository) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		userId := c.Get("user_id").(int)
-		servingSize := c.FormValue("serving_size")
-		numServings := c.FormValue("number_of_servings")
-		foodName := c.FormValue("food_name")
+		productName := c.FormValue("product_name")
+		nutrimentsStr := c.FormValue("nutriments")
+		servingSize, err := strconv.ParseFloat(c.FormValue("serving_size"), 64)
+		if err != nil {
+			fmt.Println(err)
+		}
 
-		fmt.Println(userId, foodName, servingSize, numServings)
+		numberOfServings, err := strconv.ParseFloat(c.FormValue("number_of_servings"), 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		var nutriments diet.Nutriments
+
+		err = json.Unmarshal([]byte(nutrimentsStr), &nutriments)
+		if err != nil {
+			fmt.Println("Error unmarshalling JSON:", err)
+		}
+
+		entry := data.FoodLogEntry{
+			FoodName:         productName,
+			UserID:           userId,
+			Calories:         nutriments.EnergyKcalServing * numberOfServings,
+			Protein:          nutriments.ProteinsServing * numberOfServings,
+			Carbs:            nutriments.CarbohydratesServing * numberOfServings,
+			Fats:             nutriments.FatServing * numberOfServings,
+			ServingSize:      servingSize,
+			NumberOfServings: numberOfServings,
+		}
+
+		_, err = repo.CreateFoodLogEntry(userId, entry)
+		if err != nil {
+			fmt.Println(err)
+		}
 
 		return render(c, templates.FoodLogSuccess())
+	}
+}
+
+func FoodLogDELETEHandler(repo *data.FoodLogRepository) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		entryId, err := strconv.ParseInt(c.Param("id"), 10, 0)
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+		err = repo.DeleteFoodLogEntry(int(entryId))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		return nil
 	}
 }
