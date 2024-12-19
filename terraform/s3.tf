a1e31b23b86327c08e88ed482d3fd2b2cc2945cd
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-12-19 17:23:19 +0200

    add s3

---
+++ terraform/s3.tf
@@ -0,0 +1,43 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket        = "dietapp-static"
+  force_destroy = true
+
+}
+
+resource "aws_s3_bucket_public_access_block" "block_settings" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_cors_configuration" "static_assets" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = [var.S3_ALLOWED_ORIGIN]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_policy" "static_assets" {
+  bucket     = aws_s3_bucket.s3_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.block_settings]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.s3_bucket.arn}/*"
+      },
+    ]
+  })
+}
