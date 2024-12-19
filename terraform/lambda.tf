8c4a46212bf2ca1f64718f56e7cc316b9931a3d7
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-12-19 17:15:14 +0200

    add nutritionix api credentials

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -12,6 +12,8 @@ resource "aws_lambda_function" "lambda_func" {
       AWS_LWA_PORT = var.AWS_LWA_PORT
       TURSO_TOKEN  = var.TURSO_TOKEN
       TURSO_URL    = var.TURSO_URL
+      NUTRITIONIX_APP_ID = var.NUTRITIONIX_APP_ID
+      NUTRITIONIX_APP_KEY = var.NUTRITIONIX_APP_KEY
       ENV          = var.ENV
     }
   }
