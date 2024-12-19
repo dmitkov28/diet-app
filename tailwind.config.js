aed02beaa7b3246cd76efc478d38500aad847931
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-12-19 17:11:56 +0200

    add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./templates/**/*.templ", "./templates/*.templ"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shake: "shake 1s ease-in-out",
+      },
+      keyframes: {
+        shake: {
+          "0%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(-30deg)",
+          },
+          "75%": {
+            transform: "rotate(30deg)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
