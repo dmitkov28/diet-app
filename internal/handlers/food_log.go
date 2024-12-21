95e27312e43f8ec7ad1cacf616deb824501519b0
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-12-21 10:56:12 +0200

    update food log handler

--- internal/handlers/food_log.go
+++ internal/handlers/food_log.go
@@ -1,13 +1,11 @@
 package handlers
 
 import (
-	"encoding/json"
 	"fmt"
 	"strconv"
 	"time"
 
 	"github.com/dmitkov28/dietapp/internal/data"
-	"github.com/dmitkov28/dietapp/internal/diet"
 	"github.com/dmitkov28/dietapp/templates"
 	"github.com/labstack/echo/v4"
 )
@@ -77,41 +75,7 @@ func FoodLogRefreshTotalsGETHandler(repo *data.FoodLogRepository, settingsRepo *
 func FoodLogPOSTHandler(repo *data.FoodLogRepository, settingsRepo *data.SettingsRepository) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		userId := c.Get("user_id").(int)
-		productName := c.FormValue("product_name")
-		nutrimentsStr := c.FormValue("nutriments")
-		servingSize, err := strconv.ParseFloat(c.FormValue("serving_size"), 64)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		numberOfServings, err := strconv.ParseFloat(c.FormValue("number_of_servings"), 64)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		var nutriments diet.Nutriments
-
-		err = json.Unmarshal([]byte(nutrimentsStr), &nutriments)
-		if err != nil {
-			fmt.Println("Error unmarshalling JSON:", err)
-		}
-
-		entry := data.FoodLogEntry{
-			FoodName:         productName,
-			UserID:           userId,
-			Calories:         nutriments.EnergyKcalServing * numberOfServings,
-			Protein:          nutriments.ProteinsServing * numberOfServings,
-			Carbs:            nutriments.CarbohydratesServing * numberOfServings,
-			Fats:             nutriments.FatServing * numberOfServings,
-			ServingSize:      servingSize,
-			NumberOfServings: numberOfServings,
-		}
-
-		_, err = repo.CreateFoodLogEntry(userId, entry)
-		if err != nil {
-			fmt.Println(err)
-		}
-
+		fmt.Println(userId)
 		return render(c, templates.FoodLogSuccess())
 	}
 }
